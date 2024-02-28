d664ab94841a2b4ad290f4f237d49f3c8ce52a5c
Jessel Sookha <128137223+jesselsookha@users.noreply.github.com>
2024-02-28 10:24:37 +0200

    code1 update
    recap of chapter 1 notes and a simple introduction to JavaScript code execution

---
+++ code1/code1.js
@@ -0,0 +1,14 @@
+/*
+	Program: JavaScript Example 1 
+	Programmer: Jessel Sookha 
+	Date: 27 February 2024 
+	Description: First program that we experimented with in class. 
+    Yoh! This is confusing, but we are slowly going to learn and understand this. 
+*/
+
+// This is a comment 
+// line 
+
+alert("Hello");
+
+console.log("World");
